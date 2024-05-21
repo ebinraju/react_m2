0f0e4df9f51f39d1ac563f8745c894dd791d7b20
Ebin.r <ebin.r@thinkpalm.com>
2024-05-21 11:46:06 +0530

    fix in app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from "react-error-boundary";
 import Dashboard from './components/dashboard';
