74ff2dbc7e52f5d730b20548eb7a74c660b8aafd
Ebin.r <ebin.r@thinkpalm.com>
2024-05-21 12:01:50 +0530

    fix in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { ErrorBoundary } from "react-error-boundary";
 import Dashboard from './components/dashboard';
 import HomePage from './components/homePage';
 import Login from './components/login';
 import Navigation from './components/navigation';
 import Unauthorized from './components/ErrorPages/Unauthorized';
-import ApplicationError from './components/ErrorPages/ApplicationError';
-import { AuthProvider } from './components/authentication/AuthProvider';
 import "./scss/main.scss";
 import "./input.css";
 
 function App() {
   return (
     <div className="App">
-      <AuthProvider>
         <BrowserRouter>
-          <ErrorBoundary
-            // key={location.pathname}
-            FallbackComponent={() => <ApplicationError />}
-          >
             <Routes>
               <Route path='/' element={<Navigation />}>
                 <Route path="/home" element={<HomePage />} />
@@ -28,9 +20,7 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/unauthorized" element={<Unauthorized />} />
             </Routes>
-          </ErrorBoundary>
         </BrowserRouter>
-      </AuthProvider>
     </div>
   );
 }
